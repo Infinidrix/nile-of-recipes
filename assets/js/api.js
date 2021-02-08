40e323dc1421bb766f474a0699686866c112adb6
Infinidrix <biruksolomon11@gmail.com>
2021-02-08 15:33:41 +0300

    add api implementation

---
+++ assets/js/api.js
@@ -0,0 +1,52 @@
+/**
+ * Currently puts the api key in the git repo,
+ * will change when better solution is found
+ */
+const TOKEN = "854efe0ebae543d5b88fd53f03852b6b";
+
+
+// Gets a list of random recipes, specify number or default is used 
+export async function getFeatured(amount = 6){
+    
+    let response = await fetch('https://api.spoonacular.com/recipes/random?' + new URLSearchParams({
+        number: amount,
+        limitLicense: true,
+        apiKey: TOKEN
+    }));
+
+    let data = await response.json();
+
+    return data
+    
+}
+// Test the above function UNCOMMENT BELOW CODE - should return an array of 6 recipes
+// getFeatured().then(console.log)
+
+// Gets detailed info 
+// ID is in recipe.id
+export async function getDetailed(id){
+    let response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?` + new URLSearchParams({
+        apiKey: TOKEN
+    }));
+
+    let data = await response.json();
+
+    return data
+}
+// test the above function - UNCOMMENT BELOW CODE - shoule return details of "Slow Cooker Poblano Corn Chowder with Chicken and Chorizo"
+// getDetailed(660297).then(console.log)
+
+// Pass a search query ( as a string ) and get results
+export async function searchRecipe(searchQuery){
+    //
+    let response = await fetch('https://api.spoonacular.com/recipes/complexSearch?' + new URLSearchParams({
+        query: searchQuery,
+        apiKey: TOKEN
+    }));
+
+    let data = await response.json();
+
+    return data
+}
+// test the above function - UNCOMMENT BELOW CODE - shoule return recipies related to bananas
+// searchRecipe("Banana").then(console.log)
