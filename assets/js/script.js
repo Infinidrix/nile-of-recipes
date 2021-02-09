f3e900f02eacb41e6797ea2e3f09377db14c4de5
Biruk Solomon <41226525+Infinidrix@users.noreply.github.com>
2021-02-09 11:24:53 +0300

    Biruk.web components (#6)
    * [api] put api data to display
    
    * fix css for templates
    
    * fix issue where about us cards are affected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
 // import api functions
 import { searchRecipe, getDetailed, getFeatured } from "./api.js"
+
+document.addEventListener("DOMContentLoaded", async ()=>{
+    let recipes = await getFeatured(3);
+    
+    const featuredFragment = document.querySelector('#recipe-random');
+
+    recipes.recipes.forEach(recipe => {
+        // Create an instance of the template content
+        const instance = document.importNode(featuredFragment.content, true);
+        // Add relevant content to the template
+        instance.querySelector('.card-title').innerHTML = recipe.title;
+        instance.querySelector('.card-text').innerHTML = recipe.summary;
+        instance.querySelector('.card-img-top').src = recipe.image;
+        // Append the instance ot the DOM
+        document.getElementById('content-view').appendChild(instance);
+    });
+
+    recipes = await getFeatured(3);
+
+    recipes.recipes.forEach(recipe => {
+        // Create an instance of the template content
+        const instance = document.importNode(featuredFragment.content, true);
+        // Add relevant content to the template
+        instance.querySelector('.card-title').innerHTML = recipe.title;
+        instance.querySelector('.card-text').innerHTML = recipe.summary;
+        instance.querySelector('.card-img-top').src = recipe.image;
+        // Append the instance ot the DOM
+        document.getElementById('random-recipes').appendChild(instance);
+    });
+})
