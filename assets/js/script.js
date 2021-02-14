46d57cb05dd9a36daf1fe59aebc012da222c1367
Biruk Solomon <41226525+Infinidrix@users.noreply.github.com>
2021-02-14 06:45:41 +0300

    Biruk.live data (#8)
    * Add search feature from api
    
    * add ingredients to detail page
    
    * Put common tasks on script js
    
    * finish detail page live data
    
    * add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,10 @@
 // import api functions
 import { searchRecipe, getDetailed, getFeatured } from "./api.js"
 
-document.addEventListener("DOMContentLoaded", async ()=>{
-    let recipes = await getFeatured(3);
-    
-    const featuredFragment = document.querySelector('#recipe-random');
-
-    recipes.recipes.forEach(recipe => {
-        // Create an instance of the template content
-        const instance = document.importNode(featuredFragment.content, true);
-        // Add relevant content to the template
-        instance.querySelector('.card-title').innerHTML = recipe.title;
-        instance.querySelector('.card-text').innerHTML = recipe.summary;
-        instance.querySelector('.card-img-top').src = recipe.image;
-        // Append the instance ot the DOM
-        document.getElementById('content-view').appendChild(instance);
-    });
-
-    recipes = await getFeatured(3);
-
-    recipes.recipes.forEach(recipe => {
-        // Create an instance of the template content
-        const instance = document.importNode(featuredFragment.content, true);
-        // Add relevant content to the template
-        instance.querySelector('.card-title').innerHTML = recipe.title;
-        instance.querySelector('.card-text').innerHTML = recipe.summary;
-        instance.querySelector('.card-img-top').src = recipe.image;
-        // Append the instance ot the DOM
-        document.getElementById('random-recipes').appendChild(instance);
-    });
-})
+// make the serach button go to the seach page with query variable set to the input text
+const navbarSearch = document.querySelector("#navbar-search");
+navbarSearch.querySelector("button").onclick = (e) => {
+    e.preventDefault()
+    console.log(`/search?query=${navbarSearch.querySelector("input").value}`);
+    window.location.href = `./search.html?query=${navbarSearch.querySelector("input").value}`
+};
